4ea8e7da77e85aea1a43819039d5fad2d333aa0f
Mark Malström <mark@malstrom.me>
2020-07-28 19:22:35 -0500

    Initial Commit

---
+++ SafariSplashScreen/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  SafariSplashScreen
+//
+//  Created by Mark Malstrom on 7/28/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!").padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SafariSplashScreen/SafariSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SafariSplashScreenApp.swift
+//  SafariSplashScreen
+//
+//  Created by Mark Malstrom on 7/28/20.
+//
+
+import SwiftUI
+
+@main
+struct SafariSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
